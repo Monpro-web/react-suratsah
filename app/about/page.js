37c2b6930b6efe67f0a53328aab62f908e817ff0
Tsolmon <japamon3@gmail.com>
2025-03-06 12:03:37 +0900

    ⚡️ export inport - ыг нэг хэвийн бичилт болголоо

--- app/about/page.js
+++ app/about/page.js
@@ -4,17 +4,16 @@ import { Footer } from '../components/Footer'
 import { Links } from '../components/Links'
 import { Headline } from '../components/Headline'
 
-const page = () => {
+export default page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8   sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col  row-start-2 items-center sm:items-start">
     
         <Headline  page="About" />
-        <Links />
+        <Links page="About" />
         <Footer />
       </main>  
     </div>
   )
 }
 
-export default page
--- app/components/Headline.jsx
+++ app/components/Headline.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Headline = (props) => {
-    console.log(props);
+export function Headline (props) {
+    
     
   return (
     <div>
